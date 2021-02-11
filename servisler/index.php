af36c2eaf2efe7e4ff6248a41602a9ac86c2b9ce
kerem3338 <73388608+kerem3338@users.noreply.github.com>
2021-02-11 15:25:01 +0300

    Update and rename index.md to index.php

---
+++ servisler/index.php
@@ -0,0 +1,7 @@
+<?php
+ 
+$isimler=["Ahmet","Mehmet","Hüseyin","Kemal","Deniz"]; //5 elemanlı dizi tanımlama
+$isimler[3]="Murat"; //index numarası 3 olan kaydı değiştirme
+echo $isimler[0]; //ilk indexi yazdırma
+ 
+?>
